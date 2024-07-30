5d5316714de038c46d6f02572f3a21923c3bb07f
naila <nailahafidhah2@gmail.com>
2024-07-30 15:30:12 +0800

    tiket : first commit

--- tiket.cpp
+++ tiket.cpp
@@ -2,8 +2,10 @@
 #include <cmath>
 using namespace std;
 
-double hitungBiayaTiket(int jumlahStasiun) 
+double hitungBiayaTiket(int jumlahStasiun) {
     const double tarifPerStasiun = 5000.0;
     double totalBiaya = jumlahStasiun * tarifPerStasiun;
     
-  
+  return totalBiaya;
+}
+
