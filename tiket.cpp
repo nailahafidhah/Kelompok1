ca15b4171163d8d0f59c92a9218043341e9a1b25
naila <nailahafidhah2@gmail.com>
2024-07-30 15:33:31 +0800

    tiket : first commit

--- tiket.cpp
+++ tiket.cpp
@@ -17,3 +17,30 @@ int main() {
     int stasiunAwal, stasiunTujuan;
 
       cout << "Selamat Datang di stasiun Indonesia" << endl;
+      
+ cout << "Silahkan pilih tujuan anda" << endl;
+    cout << "Tarif Per-Stasiun adalah Rp5000" << endl;
+    cout << "1. Stasiun A" << endl;
+    cout << "2. Stasiun B" << endl;
+    cout << "3. Stasiun C" << endl;
+    cout << "4. Stasiun D" << endl;
+    cout << "5. Stasiun E" << endl;
+
+    cout << endl;
+    
+    cout << "Stasiun awal: ";
+    cin >> stasiunAwal;
+    
+    if (!validStasiun(stasiunAwal)) {
+        cout << "Stasiun yang anda pilih tidak ada." << endl;
+        return 1;
+    }
+    
+    cout << "Stasiun tujuan: ";
+    cin >> stasiunTujuan;
+    
+    if (!validStasiun(stasiunTujuan)) {
+        cout << "Stasiun yang anda tuju tidak ada." << endl;
+        return 1;
+    }
+    
